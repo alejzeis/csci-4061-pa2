c77727e68a34ba358f97f3de2776f4f2173af681
alejzeis <5985254+alejzeis@users.noreply.github.com>
2023-10-18 17:52:47 -0500

    Working intermediate submission

---
+++ README.md
@@ -0,0 +1,42 @@
+# CSCI4061 PA1
+
+**Project Group**: 133
+
+**Group Members**:
+- Alejandro Zeise (zeise035)
+- Charlie Youngren (youn2835)
+
+**Device Tested On**: Personal Laptop, running Fedora 38 (Linux 6.4.15, GCC 13.2.1)
+
+**Existing File Changes**:
+- None 
+
+**Contributions:**
+- Alejandro: ```leaf_process.c```
+
+## Plan
+1. The root process parses it's arguments, creates a pipe and forks itself. The child will call exec() on the first nonleaf_process.
+2. Each nonleaf_process will do the following:
+```
+int pipeToParent[2]
+int current_pipe[2];
+pid_t pids[];
+foreach element in directory:
+    current_pipe = create_new_pipe()
+    if isFile(element):
+        pids <- fork()
+        if child:
+            // Pass pipe FD, file path
+            exec(leaf process)
+        else:
+            close(current_pipe.write_end)
+
+            data = read(new_pipe)
+            write(pipeToParent.write_end, data)
+    elseif isDirectory(element):
+        // Same thing as above, instead exec a nonleaf_process
+
+// Wait for all children to quit
+foreach pid in pids:
+    waitforpid(pid)
+```
--- src/leaf_process.c
+++ src/leaf_process.c
@@ -2,36 +2,101 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #include "../include/hash.h"
 #include "../include/utils.h"
+#include <stdbool.h>
 
-char *output_file_folder = "output/inter_submission/";
+#define HASH_SIZE (SHA256_BLOCK_SIZE * 2 + 1)
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
+static const char *output_file_folder = "output/inter_submission/";
+
+void doIntermediateSubmission(char *filePath, char *strToParent, bool attemptMkdirIfFail);
+
+void doIntermediateSubmission(char *filePath, char *strToParent, bool attemptMkdirIfFail)
+{
+    FILE *outputFile;
+    char *fileName;
+    char *rootDirectory;
+    char constructedFilePath[PATH_MAX];
+    char mkdirPath[PATH_MAX];
+
+    fileName = extract_filename(filePath);
+    rootDirectory = extract_root_directory(filePath);
+
+    // Construct path to output file
+    sprintf(constructedFilePath, "%s/%s/%s", output_file_folder, rootDirectory, fileName);
+
+    outputFile = fopen(constructedFilePath, "w");
+    if (NULL == outputFile && attemptMkdirIfFail)
+    {
+        // Output Directories probably do not exist, attempt to create it
+        
+        sprintf(mkdirPath, "%s%s", output_file_folder, rootDirectory);
+        mkdir("output", 0770);
+        mkdir("output/inter_submission", 0770);
+        mkdir(mkdirPath, 0770);
+
+        // Retry to create the output file
+        doIntermediateSubmission(filePath, strToParent, false);
+    }
+    else if(NULL == outputFile)
+    {
+        fprintf(stderr, "Failed to open output file: %s\n", constructedFilePath);
+    }
+    else
+    {
+        fprintf(outputFile, "%s", strToParent);
+
+        fclose(outputFile);
+    }
+
+    // Free root directory string, allocated in extract_root_directory()
+    free(rootDirectory);
+}
+
+int main(int argc, char* argv[]) 
+{
+    char hash[HASH_SIZE];
+
+    char strToParent[BUFFER_SIZE];
+    char *filePath;
+    int pipeWriteEnd;
+
+    memset(hash, 0, HASH_SIZE);
+
+    if (argc != 3) 
+    {
         printf("Usage: Inter Submission --> ./leaf_process <file_path> 0\n");
         printf("Usage: Final Submission --> ./leaf_process <file_path> <pipe_write_end>\n");
         return -1;
     }
-    //TODO(): get <file_path> <pipe_write_end> from argv[]
+    //(): get <file_path> <pipe_write_end> from argv[]
+    filePath = argv[1];
+    pipeWriteEnd = atoi(argv[2]);
 
 
-    //TODO(): create the hash of given file
+    //(): create the hash of given file
+    hash_data_block(hash, filePath);
 
 
-    //TODO(): construct string write to pipe. The format is "<file_path>|<hash_value>"
+    //(): construct string write to pipe. The format is "<file_path>|<hash_value>"
+    sprintf(strToParent, "%s|%s|", filePath, hash);
 
-
-    if(){
-        //TODO(inter submission)
-        //TODO(overview): create a file in output_file_folder("output/inter_submission/root*") and write the constructed string to the file
-        //TODO(step1): extract the file_name from file_path using extract_filename() in utils.c
-        //TODO(step2): extract the root directory(e.g. root1 or root2 or root3) from file_path using extract_root_directory() in utils.c
-        //TODO(step3): get the location of the new file (e.g. "output/inter_submission/root1" or "output/inter_submission/root2" or "output/inter_submission/root3")
-        //TODO(step4): create and write to file, and then close file
-        //TODO(step5): free any arrays that are allocated using malloc!! Free the string returned from extract_root_directory()!! It is allocated using malloc in extract_root_directory()
-
-    }else{
+    if (1) 
+    {
+        //(inter submission)
+        //(overview): create a file in output_file_folder("output/inter_submission/root*") and write the constructed string to the file
+        //(step1): extract the file_name from file_path using extract_filename() in utils.c
+        //(step2): extract the root directory(e.g. root1 or root2 or root3) from file_path using extract_root_directory() in utils.c
+        //(step3): get the location of the new file (e.g. "output/inter_submission/root1" or "output/inter_submission/root2" or "output/inter_submission/root3")
+        //(step4): create and write to file, and then close file
+        //(step5): free any arrays that are allocated using malloc!! Free the string returned from extract_root_directory()!! It is allocated using malloc in extract_root_directory()
+    
+        doIntermediateSubmission(filePath, strToParent, true);
+    }
+    else
+    {
         //TODO(final submission): write the string to pipe
 
         exit(0);
